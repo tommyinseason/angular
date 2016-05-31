59135f0da123a579d7c16b5314938163646fbc56
Tommy Jones <Guest@epicodus-27.local>
2016-05-31 16:46:37 -0700

    Working through angular forms

---
+++ app/task.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskComponent } from './task.component';
+import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+  `
+})
+export class TaskComponent {
+  public task: Task;
+}
+
+@Component({
+  selector: 'task-list',
+  inputs: ['tasklist'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent, EditTaskDetailsComponent],
+  template:`
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  <edit-task-details [task]="selectedTask">
+  </edit-task-details>
+  `
+})
+
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
